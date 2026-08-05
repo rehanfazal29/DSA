895d976fb3c50e5c47af8787f4fa4d518342c8ce
REHAN FAZAL <rehanfazal570@gmail.com>
2026-08-05 17:03:09 +0530

    Time: 1 ms (30.86%) | Memory: 42.3 MB (82.45%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        while (n > 0) {
+            int rem = n % 2;
+            n = n / 2;
+            if (rem == 1) {
+                count++;
+            }
+        }
+
+        return count;
+
+    }
+}
