d20ebf58ec155469e7cd12247036941d274971bd
REHAN FAZAL <rehanfazal570@gmail.com>
2026-08-06 22:10:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (50.32%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,22 +2,10 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
 
-        int count = 0;
-
         if (n <= 0) {
             return false;
         }
 
-        while (n != 0) {
-            n = n & (n - 1);
-
-            count++;
-        }
-
-        if (count == 1) {
-            return true;
-        }
-
-        return false;
+        return (n & (n - 1)) == 0;
     }
 };
